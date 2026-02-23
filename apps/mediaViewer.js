d248361e0a7a022009dcf7b3b2d5aeb0d1cfb988
gpu-maker <32omelada@sandiaprep.org>
2026-02-23 12:55:07 -0700

    Create mediaViewer.js

---
+++ apps/mediaViewer.js
@@ -0,0 +1,5 @@
+export function openMedia() {
+  window.printToTerminal("MEDIA ARCHIVE:");
+  window.printToTerminal("video_01: corrupted footage of empty room");
+  window.printToTerminal("image_04: unidentified silhouette");
+}
