14016b3ca8c22b452a25c38b8070898822546440
gpu-maker <32omelada@sandiaprep.org>
2026-02-23 16:29:40 -0700

    Update chatApp.js

--- apps/chatApp.js
+++ apps/chatApp.js
@@ -2,15 +2,11 @@ import { respond } from "../entity/chatEngine.js";
 
 export function openChat() {
   window.printToTerminal("ENTITY CHAT ACTIVE");
-  window.printToTerminal("type: talk('<message>') or just type messages");
+  window.printToTerminal("Type your messages. Entity will respond intelligently.");
 
   return {
     command: cmd => {
-      let msg = cmd;
-      if (cmd.startsWith("talk(") && cmd.endsWith(")")) {
-        msg = cmd.slice(5, -1).replace(/['"]/g, "");
-      }
-      const reply = respond(msg);
+      const reply = respond(cmd);
       setTimeout(() => window.printToTerminal("ENTITY: " + reply), 700);
     }
   };
