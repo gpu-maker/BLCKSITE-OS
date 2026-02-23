3fe7dd7c40fd2470f0cc9e55714ca76f1a7de225
gpu-maker <32omelada@sandiaprep.org>
2026-02-23 12:34:33 -0700

    Create windowManager.js

---
+++ core/windowManager.js
@@ -0,0 +1,33 @@
+import { openFiles } from "../apps/fileExplorer.js";
+import { openMonitor } from "../apps/systemMonitor.js";
+import { openNotes } from "../apps/notes.js";
+import { openMedia } from "../apps/mediaViewer.js";
+import { openBrowser } from "../apps/browser.js";
+import { openChat } from "../apps/chatApp.js";
+
+const appMap = {
+  files: openFiles,
+  monitor: openMonitor,
+  notes: openNotes,
+  media: openMedia,
+  browser: openBrowser,
+  chat: openChat
+};
+
+export function renderAppsBar() {
+  const bar = document.getElementById("apps-bar");
+  bar.innerHTML = "";
+
+  Object.keys(appMap).forEach(appName => {
+    const span = document.createElement("span");
+    span.textContent = appName.toUpperCase();
+    span.className = "app-word";
+    span.onclick = () => launchApp(appName);
+    bar.appendChild(span);
+  });
+}
+
+export function launchApp(name) {
+  if (appMap[name]) appMap[name]();
+  else window.printToTerminal("UNKNOWN APP");
+}
